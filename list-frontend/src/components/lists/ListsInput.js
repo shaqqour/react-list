b5fdfb5f837b39d95ee1186a8b4e531b3bbb84f6
Shaqqour <mshaqqourm@gmail.com>
2020-06-13 16:46:40 -0700

    Adds basic ListContainer and ListsInput

---
+++ list-frontend/src/components/lists/ListsInput.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class ListInput extends Component {
+
+    state ={
+        name: ""
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+
+}
+
+export default ListInput;
---
+++ list-frontend/src/containers/ListsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ListInput from "../components/lists/ListsInput";
+
+class ListsContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                <ListInput />
+            </div>
+        )
+    }
+
+}
+
+export default ListsContainer;
