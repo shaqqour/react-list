a805c790e4bc5e41c2fb9b47a32d40d7d0812a6d
Shaqqour <mshaqqourm@gmail.com>
2020-06-16 11:13:40 -0700

    Add List component and render it from Lists.js

--- list-frontend/src/components/lists/Lists.js
+++ list-frontend/src/components/lists/Lists.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import List from './List';
 
 const Lists = ({lists}) => {
 
     return (
         <div>
-            {console.log(lists)}
+            <List />
         </div>
     )
 
