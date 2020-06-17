9d14f46095ebffcfaf03b3398ae2e38ec8cc4c75
Shaqqour <mshaqqourm@gmail.com>
2020-06-17 16:24:43 -0700

    Change List.js to a class component

--- list-frontend/src/components/lists/List.js
+++ list-frontend/src/components/lists/List.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const List = ({list}) => {
-    return (
-        <main>
-            <div className="list" id={list.id}>
-            </div>
-            <div className="list doing" id={list.id + "doing"}>
-            </div>
-            <div className="list done" id={list.id + "done"}>
-            </div>
-        </main>
-    )
+class List extends Component {
+
+    render() {
+
+        const list = this.props.list;
+
+        return (
+            <main>
+                <div className="list" id={list.id}></div>
+                <div className="list doing" id={list.id + "doing"}></div>
+                <div className="list done" id={list.id + "done"}></div>
+            </main>
+        )
+
+    }
+
 }
 
 export default List
