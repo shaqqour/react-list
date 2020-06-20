33ea6f30e58e7d6b5c705027488021e69b673a11
Shaqqour <mshaqqourm@gmail.com>
2020-06-19 18:44:35 -0700

    Add handleChange and handleSubmit methods in ItemInput.js

--- list-frontend/src/components/items/ItemInput.js
+++ list-frontend/src/components/items/ItemInput.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 
 class ItemInput extends Component {
 
+    state ={
+        name: "",
+        status: "",
+        priority: null,
+        dueDate: null,
+        listId: null
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            name: ""
+        })
+    }
+
     render() {
         const list = this.props.list
         return (
             <div>
-                <form>
-                    <input type="text"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.name} onChange={this.handleChange}/>
                     <input className="submit" type="submit" value="Add"/>
                 </form>
             </div>
